e499b2630cadaa86f30000263396b6226112ebe7
Anhelina Priadko <anhelina.pryadko@gmail.com>
2026-03-02 19:34:17 +0000

    add authentication

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,9 +1,25 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
+import ReactDOM from 'react-dom';
+import React from 'react';
 import App from "./components/App";
+import { AuthClient } from '@dfinity/auth-client';
 
-const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    await renderApp(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "http://umunu-kh777-77774-qaaca-cai.localhost:8000/#authorize",
+      onSuccess: () => {
+        renderApp(authClient);
+      },
+    });
+  }
+};
+
+async function renderApp(authClient) {
+  ReactDOM.render(<App authClient={authClient} />, document.getElementById("root"));
 }
 
 init();
